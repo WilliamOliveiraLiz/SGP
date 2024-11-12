0f0412ccdcfeecacf9f1587621ea2565c533f1a5
william_liz <william_liz@estudante.sesisenai.org.br>
2024-11-11 21:12:09 -0300

    gerenciadorProdutos (incompleto)

--- Loja/src/Gerenciador/GerenciadorProdutos.java
+++ Loja/src/Gerenciador/GerenciadorProdutos.java
@@ -1,4 +1,29 @@
 package Gerenciador;
 
-public class GerenciadorProdutos {
+import modelo.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorProdutos extends Produto {
+    private List<Produto> produtos = new ArrayList<>();
+    private Integer proximoID = 1;
+
+    public GerenciadorProdutos(String nome, Double preco, String categoria) {
+        super(nome, preco, categoria);
+    }
+    public void criar(Produto produto){
+        validarProduto(produto);
+        produto.setId(proximoID++);
+        produtos.add(produto);
+    }
+    private void validarProduto(Produto produto) {
+        if (produto.getNome()==null) {
+            System.out.println("Não foi possivel registrar o cadastro, nome não informado!");
+        }else if (produto.getPreco()==null) {
+            System.out.println("Não foi possivel registrar o cadastro, preço não informado!\"");
+        } else {
+            
+        }
+    }
 }
